d26457ae47b21745148f308371c5071fab34f1fe
Gustaf Jarnling <gustaf@it24develops.com>
2017-12-05 16:50:33 +0100

    Updated FAQ to include Actions

--- imports/client/layouts/Faq.js
+++ imports/client/layouts/Faq.js
@@ -4,6 +4,8 @@ import NavBar from '../components/menus/nav-bar/NavBar';
 import SimpleHeader from '../components/partials/SimpleHeader';
 import FaqsListContainer from '../components/faqs/FaqsListContainer';
 import Seperator from '../components/partials/Seperator';
+import ActionsBigButtonContainer from '../components/actions/big/button/ActionsBigButtonContainer';
+import ActionsSmallContainer from '../components/actions/small/ActionsSmallContainer';
 import Footer from '../components/menus/footer/Footer';
 
 export default () => {
@@ -13,6 +15,8 @@ export default () => {
       <SimpleHeader title="Frequently Asked Questions"/>
       <FaqsListContainer/>
       <Seperator/>
+      <ActionsBigButtonContainer/>
+      <ActionsSmallContainer/>
       <Footer/>
     </div>
   );
