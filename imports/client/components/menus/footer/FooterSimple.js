89db363f2b645e58db56a83f3cd27891b6b4974a
Gustaf Jarnling <gustaf@it24develops.com>
2017-12-05 14:12:56 +0100

    Updated NotFound

---
+++ imports/client/components/menus/footer/FooterSimple.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <a href="/" className="button-2 w-inline-block">
+      <div className="container w-container">
+        <div className="button-2-component-1">
+          <div>
+            <div className="button-2-component-2">
+              <div className="button-2-component-3">
+                <h4>Continue to the home page</h4>
+              </div>
+              <div>
+                <div className="icon-wrapper-1">
+                  <img src="images/arrow-3x_1.png" width="23" className="icon-pic-1"/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
--- imports/client/layouts/NotFound.js
+++ imports/client/layouts/NotFound.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
+import NavBar from '../components/menus/nav-bar/NavBar';
+import FooterSimple from '../components/menus/footer/FooterSimple';
+
 export default () => {
   return (
-    <div>
-      <h1>Monthly Cup</h1>
-      <p>Page not found!</p>
-      <p>Links:</p>
-      <a href="/">
-        <div>Home</div>
-      </a>
-      <a href="/faq">
-        <div>FAQ</div>
-      </a>
+    <div className="section-4">
+      <NavBar/>
+      <div className="section-1">
+        <div className="container w-container">
+          <div className="grid-wrapper-7">
+            <div className="content-component-6">
+              <h1>404</h1>
+            </div>
+            <div className="content-component-6">
+              <img src="images/lost3x.png" srcSet="images/lost3x-p-500.png 500w, images/lost3x.png 717w" sizes="(max-width: 479px) 100vw, 210px" className="content-component-7"/>
+            </div>
+            <div className="content-component-6">
+              <div>Wait, what? This page does not exist.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterSimple/>
     </div>
   );
 };
